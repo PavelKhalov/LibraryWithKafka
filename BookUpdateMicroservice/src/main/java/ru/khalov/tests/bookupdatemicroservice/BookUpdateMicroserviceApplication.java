c7ce571f2c90c868af60a9c56f2d766f7c8a3a44
PavelKhalov <hahaz2@yandex.ru>
2026-05-30 22:51:50 +0300

    append git for BookUpdateMicroservice

---
+++ BookUpdateMicroservice/src/main/java/ru/khalov/tests/bookupdatemicroservice/BookUpdateMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.khalov.tests.bookupdatemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookUpdateMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookUpdateMicroserviceApplication.class, args);
+    }
+
+}
